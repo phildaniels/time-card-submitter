6615b03d54766856edaa555b3c7db01a6bb2fe25
Phil Daniels <05879@global.ul.com>
2022-07-15 16:04:42 -0600

    installed prettier

--- functions/WeeklySubmittal/index.ts
+++ functions/WeeklySubmittal/index.ts
@@ -1,13 +1,15 @@
-import { AzureFunction, Context } from "@azure/functions"
+import { AzureFunction, Context } from "@azure/functions";
 
-const timerTrigger: AzureFunction = async function (context: Context, myTimer: any): Promise<void> {
-    var timeStamp = new Date().toISOString();
-    
-    if (myTimer.isPastDue)
-    {
-        context.log('Timer function is running late!');
-    }
-    context.log('Timer trigger function ran!', timeStamp);   
+const timerTrigger: AzureFunction = async function (
+  context: Context,
+  myTimer: any
+): Promise<void> {
+  var timeStamp = new Date().toISOString();
+
+  if (myTimer.isPastDue) {
+    context.log("Timer function is running late!");
+  }
+  context.log("Timer trigger function ran!", timeStamp);
 };
 
 export default timerTrigger;
